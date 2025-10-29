ac1c7914915ffbe9d5c49e230ecb164494e63e85
El Abbassi Ismail <eismail@c2r9p5.1337.ma>
2025-10-29 12:56:46 +0100

    add files

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://ielab036_db_user:D5xvj9TtQQb0BzLC@todoapp.ryzdzfv.mongodb.net/?appName=todoApp")
+    .then(() => console.log('connected to mongoDB'))
+    .catch(err => console.error('connected to mongoDB:',err));
+
+const todoSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    completed: {type: Boolean, default: false}
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+app.get('/todos', async (req,res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+});
+
+app.post('/todos', async (req, res) => {
+    const {title} = req.body;
+    if (!title){
+        return res.status(400).json({message: 'Title is required'});
+    }
+    const existingTodo = await Todo.findOne({title});
+    if (existingTodo)
+        return res.status(409).json({message: 'A todo with title already exists'});
+
+    const newTodo = new Todo(req.body);
+    await newTodo.save();
+
+    res.status(201).json(newTodo);
+});
+
+app.put('/todos/:id', async (req,res) => {
+    const update = await Todo.findByIdAndUpdate(req.params.id, req.body,{ new: true });
+    res.json(update);
+});
+
+app.delete('/todos/:id', async (req,res) => {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.json({message: 'Todo delete'});
+});
+
+app.listen(3000, () => console.log('is up on http://localhost:3000'));

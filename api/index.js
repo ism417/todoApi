8ae0edd1591625d65464abe0d16e46080ee76392
El Abbassi Ismail <eismail@c2r9p5.1337.ma>
2025-11-01 10:38:13 +0100

    auth3

--- api/index.js
+++ api/index.js
@@ -50,7 +50,7 @@ const Users = mongoose.model('Users', usersSchema);
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://todo-api-henna.vercel.app'
+    callbackURL: 'https://todo-api-henna.vercel.app/api/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await Users.findOne({ googleId: profile.id });
